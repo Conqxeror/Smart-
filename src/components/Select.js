fb529e49c1fbcf274280c259782750b1c469fa24
Wali Mohammad Kadri <kadriwalimohammad@gmail.com>
2023-04-06 23:44:24 +0530

    UI update

--- src/components/Select.js
+++ src/components/Select.js
@@ -5,7 +5,7 @@ export default function Select() {
     return (
         <>
             <Card className="text-center position-absolute top-50 start-50 translate-middle">
-            <Button variant="success" size="lg" className='p-70'>
+            <Button variant="success" size="lg">
                 Activate
             </Button>
             </Card>
